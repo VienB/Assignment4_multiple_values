25ad2d1b5dfcbb75131565b17e537b049847799a
VienB <vienangelobbernales@iskolarngbayan.pup.edu.ph>
2021-11-10 08:34:44 -0800

    attempt

---
+++ Program3N.py
@@ -0,0 +1,23 @@
+# Create a program which you will enter the amount of money you have, it will also ask for the price of an apple.
+# Display the maximum number of apples that you can buy and the remaining money that you will have.
+# Display the output in the following format.
+# You can buy ___ apples and your change is ___ pesos.
+
+print("Vien Angelo Bernales / BSCOE 1-1 \n")
+
+#use a function and return multiple values
+
+def YourMoney():
+    fee = int(input("how much money do you have? "))
+    price = int(input("Enter the price of the apple: "))
+    return fee, price
+
+def getTotal(moneyQ, appleQ, total):
+    TotalApples = int(moneyQ / appleQ)
+    amount = int(appleQ * total)
+    total = int(moneyQ - total)
+    return TotalApples, amount
+
+def getOutput(affordable, changeF):
+    print(f"You can buy {affordable} apples and your change is {changeF} pesos.")
+
