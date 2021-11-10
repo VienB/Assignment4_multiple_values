6a1ef0c7fd15af63ad419de78e76600e09dbd9a8
VienB <vienangelobbernales@iskolarngbayan.pup.edu.ph>
2021-11-10 06:41:51 -0800

    wrong output

--- Program2N.py
+++ Program2N.py
@@ -1,22 +1,16 @@
-# Create a program that will ask how many apples and oranges you want to buy.
-# Display the total amount you need to pay if apple price is 20 pesos and orange is 25.
-# Display the output in the following format.
-# The total amount is ______.
+def FirstGroup():
+    ApplesFunc = int(input ("Enter the quantity of apples that you want to buy: "))
+    OrangesFunc = int(input ("Enter the quantity of oranges that you want to buy: "))
+    return ApplesFunc, OrangesFunc
 
-print("Vien Angelo Bernales / BSCOE 1-1 \n")
-#use a function and return multiple values
-def AppleOrange():
-    AppleF = int(input("How many apple you want to buy? "))
-    OrangeF = int(input("How many orange you want to buy? "))
-    return AppleF, OrangeF
+def SecondGroup(Apples, Oranges):
+    PriceApplesFunc = Apples * 20
+    PriceOrangesFunc = Oranges * 25
+    return  PriceApplesFunc, PriceOrangesFunc
 
-def Calculate():
-    outcome = (AppleQ * 20) + (OrangeQ * 25)
+def FinalOutput(PriceOfApple1, PriceOfOrange1):
+    print (f'The total amount is {PriceOfApple1 + PriceOfOrange1}')
 
-def DisplayQ(PriceF):
-    print(f"The total amount is {PriceF}.")
-
-
-AppleQ, OrangeQ = AppleOrange()
-Price = Calculate()
-DisplayQ(AppleQ, OrangeQ, Price)
+Apples, Oranges = FirstGroup()
+PriceOfApple, PriceOfOrange = SecondGroup(Apples, Oranges)
+FinalOutput (PriceOfApple, PriceOfOrange)
